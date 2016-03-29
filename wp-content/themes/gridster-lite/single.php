939680658d9afc56b6e88b91c5966d972c76918d
Ofer K <ofer.k@tikalk.com>
2016-03-30 00:06:33 +0300

    Next move support

--- wp-content/themes/gridster-lite/single.php
+++ wp-content/themes/gridster-lite/single.php
@@ -10,4 +10,34 @@ get_header(); ?>
 <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('content', 'single'); ?>
 <?php endwhile; // end of the loop. ?>
+
+
+
+<?php
+//get all posts for children of category $cata
+
+
+$post = get_post($post);
+$title = isset($post->post_title) ? $post->post_title : '';
+
+query_posts(array('posts_per_page' => 5, 'category__in' => array(get_cat_ID($title))));
+while (have_posts()) {
+    the_post();
+    get_template_part('content', get_post_format($post->ID));
+} ?>
+
+<?php
+//Reset Query
+wp_reset_query();
+
+
+?>
+
+
+
+
+<?php //else : ?>
+<!--    --><?php //get_template_part('no-results', 'index'); ?>
+<?php //endif; ?>
+
 <?php get_footer(); ?>
